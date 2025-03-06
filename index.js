bd03c2eb9a912e85d3c75b6a77b5ae8d42196a4f
benson <bensonmunge545@gmail.com>
2025-03-06 12:11:18 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
+console.log(message)
+
 
 module.exports = {name, height, message};
+
+require ( './root.js' );
+
+
+
+
+
+
+/*
+describe('what-is-a-test', () => {
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual('Susan')
+    })
+  })
+
+
+  describe('Height', () => {
+    it('is less than 40', () => {
+      expect(height).toBeLessThan(40)
+    })
+  })
+
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
+    })
+  })
+})
+*/
+  
