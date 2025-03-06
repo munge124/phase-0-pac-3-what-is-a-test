a05063b7de6984774f48e3f5b6bc95f8a6e03090
benson <bensonmunge545@gmail.com>
2025-03-06 12:32:23 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
-console.log(message)
-
-
-module.exports = {name, height, message};
-
-require ( './root.js' );
 
 
 
+module.exports = {name, height, message}
+
+  console.log(message)
 
 
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
 
 
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
-    })
-  })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
-    })
-  })
-})
-*/
   
